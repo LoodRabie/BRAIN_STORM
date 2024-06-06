d901ef49aa041c847ab2073a7d9a822b18cf88a7
Lood Rabie <lood0518@gmail.com>
2024-06-06 20:31:21 +0200

    had to run migrate again

--- db/schema.rb
+++ db/schema.rb
@@ -39,5 +39,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_05_202434) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
   add_foreign_key "brains", "users"
 end
