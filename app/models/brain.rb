3393acb21fb591bdf0e8c113889d2046ee066eef
Ifeanyi Nwocha <ifeanyinwocha@gmail.com>
2024-06-06 21:16:36 +0200

    booking model done along with updated validations

--- app/models/brain.rb
+++ app/models/brain.rb
@@ -1,3 +1,9 @@
 class Brain < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :characteristics, presence: true
+  validates :description, presence: true
+  validates :specialities, presence: true
+  validates :available, inclusion: { in: [true, false] }
 end
