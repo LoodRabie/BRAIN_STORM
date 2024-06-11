10dd4d2b9287f98968ebafa82aeddee138406bf5
Lood Rabie <lood0518@gmail.com>
2024-06-11 20:05:14 +0200

    added question and answer to brains

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_124643) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_11_180356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_08_124643) do
     t.float "latitude"
     t.float "longitude"
     t.string "location"
+    t.string "question"
+    t.string "answer"
     t.index ["user_id"], name: "index_brains_on_user_id"
   end
 
